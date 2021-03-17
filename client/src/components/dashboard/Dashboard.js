3808508dbd4d46ac2a2d1a9e7e205632688699c3
birane906 <birane906@gmail.com>
2021-03-17 20:40:26 +0100

    component just added for test

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -1,7 +1,9 @@
 import { React, useState } from 'react';
 import { Route } from 'react-router';
 import { Link } from 'react-router-dom';
+import "./festival"
 import "./Dashboard.css"
+import festival from './festival';
 
 const Dashboard = (props) => {
 
@@ -16,28 +18,41 @@ const Dashboard = (props) => {
       }
 
     return (
+        
         <div>
+           
             <div id="mySidenav" className="sidenav" style={{ width: navWidth}}>
+            
                 {
                     props.children.map((value, index) => {
                         return (
+                            
                             <Link to={value.props.path} onClick={closeNav} key={index}>{value.props.title}</Link>
                         )
                     })
                 }
+                
                 <p className="closebtn" onClick={closeNav}>&times;</p>
+                
             </div>
+            
             <span style={{"fontSize":"30px","cursor":"pointer"}} onClick={openNav}>&#9776;</span>
+            
             <div className="dashboardContent">
+            
                 {
                     props.children.map((value, index) => {
                         return (
                             <Route path={value.props.path} component={() => {return value}} key={index}/>
+                            
                         )
                     })
                 }
+                
             </div>
+            
         </div>
+        
     );
 };
 
---
+++ client/src/components/dashboard/festival.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const festival = (props) => {
+    return (
+        <div>
+            <h1>{props.title}</h1>
+            <h2>MAAAAAAAAAAAAAAAAAAAAAA</h2>
+        </div>
+    );
+};
+
+export default festival;
