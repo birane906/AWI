10c362266f5e4d6e7468d006ef816928ae34280a
birane906 <birane906@gmail.com>
2021-03-18 23:05:12 +0100

    festival being developped

--- client/src/components/festival/Festivals.js
+++ client/src/components/festival/Festivals.js
@@ -5,11 +5,17 @@ import Festival from './Festival'
 function Festivals(props) {
     return (
         <div>
+        <div>
             <Festival/>
+            <br/><br/>
             <Festival/>
+            <br/><br/>
             <Festival/>
+            <br/><br/>
             <Festival/>
-            <Festival/>
+            
+        </div>
+        <button> DISPLAY A FORM TO ADD FESTI</button>
         </div>
     )
 }
--- client/src/components/festival/festival.js
+++ client/src/components/festival/festival.js
@@ -2,40 +2,32 @@ import React from 'react'
 
 function Festival() {
     return (
-        <div>
-          <div>
-          <div> <h1> *NomDuFestival*</h1> </div> 
-          <div> <h2>Année *AnnéeDuFestivalBDD*</h2> </div> 
-          </div>  
+        
+          <div className="content">
+           <h1> *NomDuFestival*</h1>  
+           <h2>Année *AnnéeDuFestivalBDD*</h2>  
+            Nombre d'emplacement Total *nbEmplTotalDuFestivalBDD*
+            <br/>
+            Prix Emplacement Premium *prix*  
+            <br/>
+            Nombre de m² *nbm2*
+            <br/>
+          Nombre de m² buvette *nbm2buvette*
+          <br/>
+            Nombre d'emplacement Premium *nbEmplTotalDuFestivalBDD*
+            <br/>
+            Prix Emplacement Premium *prix* 
+            <br/> 
+            Nombre de m² premium *nbm2p*
+            <br/>
+       
+            Nombre d'emplacement Promo *nbEmplTotalDuFestivalBDD*  
+            <br/>
+            Prix Emplacement Premium *prix*  
+          </div>
 
-          <div>
-            <div>Nombre d'emplacement Total *nbEmplTotalDuFestivalBDD*</div>
-            <div>Prix Emplacement Premium *prix*  </div>
-          </div> 
-
-          <div>
-            <div>Nombre de m² *nbm2*</div>
-          </div> 
-
-          <div>
-            <div>Nombre de m² buvette *nbm2buvette*</div>
-          </div> 
-
-          <div>
-            <div>Nombre d'emplacement Premium *nbEmplTotalDuFestivalBDD* </div>
-            <div>Prix Emplacement Premium *prix*   </div>
-          </div> 
-
-          <div>
-            <div>Nombre de m² premium *nbm2p*</div>
-          </div> 
-
-          <div>
-            <div>Nombre d'emplacement Promo *nbEmplTotalDuFestivalBDD*  </div>
-            <div>Prix Emplacement Premium *prix* </div> 
-          </div> 
-
-        </div>
+        
+        
     )
 }
 
