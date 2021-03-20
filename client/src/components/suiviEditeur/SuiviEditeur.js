00b904c7aacde8b2198b33b11c4749978a503b47
birane906 <birane906@gmail.com>
2021-03-20 15:15:55 +0100

    just get started

---
+++ client/src/components/suiviEditeur/SuiviEditeur.js
@@ -0,0 +1,31 @@
+import React from 'react'
+
+function SuiviEditeur() {
+    return (
+        <div>
+           *Nom de l'éditeur*
+           * Commentaire*
+           <div>
+            *1er contact*   
+            *2nd contact*  
+            *3eme contact* 
+            * est benevole fjmm* 
+            <input type="checkbox" />
+            * editeurs présent*
+            <input type="checkbox" />
+            *placé sur le plan ?*
+            <input type="checkbox" />
+            </div>  
+            *nb table*
+            *nb m2*
+            <div> 
+            *date emission facture*
+            *date paiement facture*
+            </div> 
+            *total a payer*
+            
+        </div>
+    )
+}
+
+export default SuiviEditeur
---
+++ client/src/components/suiviEditeur/SuiviEditeurs.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import SuiviEditeur from './SuiviEditeur'
+
+function SuiviEditeurs() {
+    return (
+        <div>
+           <h1> Suivi des éditeurs</h1> 
+           <SuiviEditeur />
+           <SuiviEditeur />
+           <SuiviEditeur />
+        </div>
+    )
+}
+
+export default SuiviEditeurs
