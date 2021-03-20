ece6844203e62092811d22d6bdc9f2d8d0467421
birane906 <birane906@gmail.com>
2021-03-20 12:28:18 +0100

    test

--- client/src/components/festival/Festivals.js
+++ client/src/components/festival/Festivals.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React, { useState } from 'react'
 import Festival from './Festival'
 
@@ -14,23 +13,8 @@ function Festivals() {
             <br/><br/>
             
         </div>
-        <button > DISPLAY A FORM TO ADD FESTI</button>
-        
-=======
-import React from 'react'
-import Festival from './Festival'
-
-
-function Festivals(props) {
-    return (
-        <div>
-            <Festival/>
-            <Festival/>
-            <Festival/>
-            <Festival/>
-            <Festival/>
->>>>>>> ab9da57225deea498b754e7fe73685aedf3dc7af
-        </div>
+        <button > DISPLAY A FORM TO ADD FESTI</button>   
+        </div>  
     )
 }
 
