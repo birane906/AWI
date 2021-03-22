5c253f3a1bf2e62880dfdcec4010a4ff801f6280
birane906 <birane906@gmail.com>
2021-03-22 22:35:06 +0100

    test

--- client/src/App.js
+++ client/src/App.js
@@ -7,6 +7,7 @@ import Dashboard from "./components/dashboard/Dashboard"
 import TestComponent from "./components/dashboard/TestComponent"
 import Festivals from "./components/festival/Festivals"
 import Jeux from "./components/jeu/Jeux"
+import Facturation from "./components/facturation/Facturation"
 
 const App = () => {
     return (
@@ -20,8 +21,7 @@ const App = () => {
                     <Dashboard>
                         <Festivals title="festivals" path="/dashboard/festivals"/>
                         <Jeux title="Jeux" path="/dashboard/jeux"/>
-                        <TestComponent title="Welcome" path="/dashboard/welcome"/>
-                        <TestComponent title="Hi" path="/dashboard/hi"/>
+                        <Facturation />
                         <TestComponent title="Hello" path="/dashboard/hello"/>
                         <TestComponent title="Sup" path="/dashboard/sup"/>
                         
---
+++ client/src/components/facturation/Facturation.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+
+function Facturation() {
+    return (
+        <div>
+            <input type="date" />
+            <input type="date" />
+        </div>
+    )
+}
+
+export default Facturation
