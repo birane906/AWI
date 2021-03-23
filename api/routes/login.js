4721bcd0751366e8726ebf2c06b2af08708e0f1f
tirgue <rara30andro@gmail.com>
2021-03-23 11:05:23 +0100

    Handle login with db

--- api/routes/login.js
+++ api/routes/login.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+var db = require('../db');
 
 router.get('/', (req, res) => {
     if (req.session.userId) {
@@ -12,12 +13,23 @@ router.get('/', (req, res) => {
 router.post('/', (req, res) => {
     const login = req.body.login
     const password = req.body.password
-    if (login == "admin" && password == "admin") {
-        req.session.userId = "admin"
-        res.status(200).send("Valid")
-    } else {
-        res.status(401).send("Invalid login or password")
-    }
+    db.query(`
+        SELECT "idUser" FROM public."User"
+        WHERE 
+                '${login}'="nameUser"
+            AND crypt('${password}',"passwordUser")="passwordUser"`)
+            .then(data => {
+                if (data.rows.length > 0) {
+                    req.session.userId = "admin"
+                    res.status(200).send("Valid")
+                } else {
+                    res.status(401).send("Invalid login or password")
+                }
+            })
+            .catch(err => {
+                console.log(err);
+                res.status(401).send("Invalid login or password")
+            })
 });
 
 module.exports = router;
