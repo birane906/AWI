c6127f56af69c1bef0cb1de5948ecef67d27e512
birane906 <birane906@gmail.com>
2021-03-27 16:14:17 +0100

    work soon

--- client/src/components/zone/Zones.js
+++ client/src/components/zone/Zones.js
@@ -22,6 +22,7 @@ const Zones = (props) => {
 
     const getJeuReserve = async()=>{
         try{
+            
             const response = await fetch("http://localhost:8080/api/listJeu");
             const jsonData = await response.json();
             setJeuReserve(jsonData);
@@ -45,69 +46,78 @@ const Zones = (props) => {
         <div>
             
             <h1> Listes des zones </h1>
-            {
-                    zones.map(zone => (
-                        jeuReserve.map(j => ( 
-                    <div> 
-                        
-                        <h2> {zone.name_zone}</h2>
-                        
-                        <h4> Jeux réservés</h4>
-                        <Button  onClick={() => setOpen(!open)} aria-controls="example-collapse-text" aria-expanded={open}>
-                                    v
-                                </Button>
-                        <Collapse in={open}>
-                        <Table  striped bordered hover size="sm">
-                            <thead>
-                                <tr>
-                                <th>Nom du jeu</th>
-                                <th>Editeur</th>
-                                <th>Joueurs</th>
-                                <th>Âge min</th>
-                                <th>Durée</th>
-                                <th>Type</th>
-                                <th>Placé ?</th>
-                                <th>Reçu ?</th>
-                                <th>A anim ?</th>
-                                </tr>
-                            </thead>
-                    
-                            <tbody>
+            {       
+                zones.map((zone, id) =>{
+                    return(
+                        jeuReserve.map((j, id) => {
+                            
+                            if(zone.id_zone == j.id_zone){
+                            return (
+                                    <div> 
+                                        
+                                        <h2> {zone.name_zone}</h2>
+                                        
+                                        <h4> Jeux réservés</h4>
+                                        <Button  onClick={() => setOpen(!open)} aria-controls="example-collapse-text" aria-expanded={open}>
+                                                    v
+                                                </Button>
+                                        <Collapse in={open}>
+                                        <Table  striped bordered hover size="sm">
+                                            <thead>
+                                                <tr>
+                                                <th>Nom du jeu</th>
+                                                <th>Editeur</th>
+                                                <th>Joueurs</th>
+                                                <th>Âge min</th>
+                                                <th>Durée</th>
+                                                <th>Type</th>
+                                                <th>Placé ?</th>
+                                                <th>Reçu ?</th>
+                                                <th>A anim ?</th>
+                                                </tr>
+                                            </thead>
+                                    
+                                            <tbody>
+                            
+                                            <tr key={j.id_zone}>
+                                            <td> {j.name_jeu}</td>
+                                            <td> {j.nom_editeur}</td>
+                                            <td> {j.nb_joueurs_min} - {j.nb_joueurs_max}</td>
+                                        
+                                            <td> {j.agemin}</td>
+                                            <td> {j.duree}</td>
+                                            <td> {j.libelle_type}</td>
+                                            {
+                                            j.place_plan
+                                                ? <td> <input type="checkbox" checked/></td>
+                                                : <td> <input type="checkbox" /></td> 
+                                            }
+                                            {
+                                            j.recu
+                                                ? <td> <input type="checkbox" checked/></td>
+                                                : <td> <input type="checkbox" /></td> 
+                                            }
+                                            {
+                                            j.anim
+                                                ? <td> <input type="checkbox" checked/></td>
+                                                : <td> <input type="checkbox" /></td> 
+                                            }         
+                                            <td> <Button variant="info">Supprimer</Button> </td>
+                                            </tr>  
+                                            
+                                            </tbody>
+                                            </Table>
+                                            </Collapse>
+                                        
+                                    </div> 
+                            )
+                        }
+                        })
+                    )               
+                })
+            }                    
+                          
             
-                            <tr key={zone.name_zone}>
-                            <td> {j.name_jeu}</td>
-                            <td> {j.nom_editeur}</td>
-                            <td> {j.nb_joueurs_min} - {j.nb_joueurs_max}</td>
-                        
-                            <td> {j.agemin}</td>
-                            <td> {j.duree}</td>
-                            <td> {j.libelle_type}</td>
-                            {
-                            j.place_plan
-                                ? <td> <input type="checkbox" checked/></td>
-                                : <td> <input type="checkbox" /></td> 
-                            }
-                            {
-                            j.recu
-                                ? <td> <input type="checkbox" checked/></td>
-                                : <td> <input type="checkbox" /></td> 
-                            }
-                            {
-                            j.anim
-                                ? <td> <input type="checkbox" checked/></td>
-                                : <td> <input type="checkbox" /></td> 
-                            }         
-                            <td> <Button variant="info">Supprimer</Button> </td>
-                            </tr>  
-                            
-                            </tbody>
-                            </Table>
-                            </Collapse>
-                            
-                    </div>
-                    ))
-                    ))           
-                }
         </div>
     );
 }
