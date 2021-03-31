21cc4085cb8ff0e9b0fef30cf9b3ebba99b372e8
birane906 <birane906@gmail.com>
2021-03-31 23:54:55 +0200

    style beginning

--- client/src/components/festival/Festival.js
+++ client/src/components/festival/Festival.js
@@ -1,26 +1,27 @@
 import React from 'react'
 import "./Festival.css"
+import { Table } from 'react-bootstrap';
 
 function Festival(props) {
   return (
-    <div className="content">
+    <Table className="content">
       <h1> {props.name}</h1>
       <h2>Année {props.year}</h2>
       {
         props.specs.map((value, index) => {
           return (
-            <div key={index}>
-              <h3>Espace</h3>
+            <Table key={index}>
+              <th>Espace</th>
               <ul>
                 <li>Nombre de tables : {value.nb_table}</li>
                 <li>Prix des tables : {value.prix_table} €</li>
                 <li>Prix des m² : {value.prix_m2} €</li>
               </ul>
-            </div>
+            </Table>
           )
         })
       }
-    </div>
+    </Table>
     )
 }
 
