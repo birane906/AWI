60af84e7711b16578aebcddd3bd8900d8361bda3
birane906 <birane906@gmail.com>
2021-04-01 09:30:58 +0200

    festivals css test

--- client/src/components/festival/Festival.js
+++ client/src/components/festival/Festival.js
@@ -4,24 +4,24 @@ import { Table } from 'react-bootstrap';
 
 function Festival(props) {
   return (
-    <Table className="content">
+    <div className="content">
       <h1> {props.name}</h1>
       <h2>Année {props.year}</h2>
       {
         props.specs.map((value, index) => {
           return (
-            <Table key={index}>
+            <div key={index}>
               <th>Espace</th>
               <ul>
                 <li>Nombre de tables : {value.nb_table}</li>
                 <li>Prix des tables : {value.prix_table} €</li>
                 <li>Prix des m² : {value.prix_m2} €</li>
               </ul>
-            </Table>
+            </div>
           )
         })
       }
-    </Table>
+    </div>
     )
 }
 
