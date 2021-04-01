f1218ce7fd2e1a0a877ed5836af431a576e31d3c
tirgue <rara30andro@gmail.com>
2021-04-01 17:15:44 +0200

    Fix routing

--- api/routes/index.js
+++ api/routes/index.js
@@ -5,8 +5,9 @@ var editeurRouteur = require('./suivi')
 var exposantRouter = require('./exposant')
 var festivalsRouter = require('./festivals')
 var loginRouter = require('./login');
-var jeuRouter = require('./listjeu');
+var listjeuRouter = require('./listjeu');
 var editeurRouter = require('./editeur')
+var jeuRouter = require('./jeu')
 
 router.get('/ping', function(req, res, next) {
   res.send({ping : "Server pinged"}); 
@@ -15,7 +16,8 @@ router.use("/suivi", editeurRouteur)
 router.use("/exposant",exposantRouter)
 router.use('/festivals', festivalsRouter)
 router.use("/login", loginRouter)
-router.use("/listjeu",jeuRouter)
+router.use("/listjeu",listjeuRouter)
+router.use("/jeu", jeuRouter)
 router.use("/zone", zoneRouter)
 router.use("/editeur",editeurRouter)
 
